6214113fd6caf02982c70e823b070a3ceab7f493
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-03-21 18:32:25 -0300

    summary and table styling

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
 import { Header } from './../../components/Header/index'
+import { Summary } from './../../components/summary/index'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburger</td>
+              <td>
+                <PriceHighlight variant='outcome'>- R$ 12,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
